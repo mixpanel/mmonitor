26c67a7f0d3e26bb7527314576e69c22db0034b0
raylu <raylu@mixpanel.com>
2011-08-22 19:31:45 -0700

    add exclude list

--- discovery/__init__.py
+++ discovery/__init__.py
@@ -5,9 +5,12 @@ def discovery(status, test):
         servers = d().get_servers() # [('ip', 'host')]
         for server in servers:
             ip = server[0]
+            host = server[1]
+            if host in settings.exclude:
+                continue
             if ip not in status['servers']: # do discovery
                 status['servers'][ip] = {}
                 logging.info('performing discovery on %r', server)
                 for t in test:
                     t.discover(ip, status['servers'][ip])
-            status['servers'][ip]['hostname'] = server[1]
+            status['servers'][ip]['hostname'] = host
--- settings.py
+++ settings.py
@@ -4,6 +4,8 @@ from tests import mongo, arb, memcache
 discovery = [softlayer.SoftLayer, rackspace.Rackspace]
 tests = [mongo.Mongo, arb.Arb, memcache.Memcache]
 
+exclude = ['tim', 'suhail', 'raylu', 'avery', 'carl', 'anlu', 'eric']
+
 smtp_host = 'localhost'
 from_addr = 'mmonitor@corp.mixpanel.com'
 to_addr = 'servers@example.com'
